06450760eb6970474f1f0bc9c0fc391e647ee4f2
Nehemiah Brown <nbrowndeveloper@gmail.com>
2026-07-30 19:31:32 -0500

    Created function to fetch current weather data, created custom object to clean data

---
+++ WeatherApp/src/components/TodaysWeatherCard.jsx
@@ -0,0 +1,7 @@
+export default function TodaysWeatherCard(){
+    return (
+        <div className="bg-[var(--card-background)] backdrop-blur-[18px] border border-2 border-[var(--border)] w-[80%] text-[var(--text-color)]">
+            <h1 className="text-2xl">Today's Weather</h1>
+        </div>
+    )
+}
--- WeatherApp/src/pages/MainPage.jsx
+++ WeatherApp/src/pages/MainPage.jsx
@@ -1,32 +1,51 @@
 import { useEffect, useState } from "react";
 
+import TodaysWeatherCard from "../components/TodaysWeatherCard.jsx"
+
 export default function MainPage() {
-  const [weatherData, setWeatherData] = useState([]);
+  const [currentWeatherData, setCurrentWeatherData] = useState([]);
   const [inputLocationName, setInputLocationName] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
   // getting forecast api for fetching forecast data & geo api for translating city names into coordinates
-  const forecastApi =
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,apparent_temperature,relative_humidity_2m,weather_code,wind_speed_10m,wind_direction_10m,is_day&hourly=temperature_2m,precipitation_probability,weather_code,visibility,uv_index,sunshine_duration,rain,precipitation&timezone=auto";
+
   const geoApi = `https://geocoding-api.open-meteo.com/v1/search?name=${inputLocationName}&count=10&language=en&format=json`;
 
   const handleInputChange = (e) => {
     setInputLocationName(e.target.value);
   };
 
-  useEffect(() => {
-    const fetchWeatherData = async () => {
+  
+    const fetchCurrentWeatherData = async (index) => {
       try {
+        const forecastApi =
+     `https://api.open-meteo.com/v1/forecast?latitude=${searchResults[index].latitude}&longitude=${searchResults[index].longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,showers_sum,snowfall_sum,precipitation_probability_max&hourly=temperature_2m,rain,snowfall,showers,relative_humidity_2m,visibility&current=temperature_2m,is_day,apparent_temperature,precipitation,rain,showers,snowfall,relative_humidity_2m&timezone=GMT&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
         const response = await fetch(forecastApi);
         const data = await response.json();
 
-        setWeatherData(data);
+        const currentWeather = {
+            temperature: data?.current?.temperature_2m,
+            feelsLike: data?.current?.apparent_temperature,
+            precipitation: data?.current?.precipitation,
+            rain: data?.current?.rain,
+            snow: data?.current?.snowfall,
+            dayOrNight: data?.current?.is_day,
+        }
+
+        setCurrentWeatherData(currentWeather);
       } catch (e) {
         console.log("Error fetching data: ", e.message);
       }
     };
-    fetchWeatherData();
-  }, []);
+ 
+
+  useEffect(() => {
+    console.log(currentWeatherData)
+  }, [currentWeatherData])
+
+   function getLocationInformation(index){
+    console.log(searchResults[index].latitude)
+  }
 
   useEffect(() => {
     const getLocationNames = async () => {
@@ -45,10 +64,11 @@ export default function MainPage() {
     getLocationNames();
   }, [geoApi, inputLocationName]);
 
+
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <div className="dashboard h-[80vh] w-[92%] bg-background-image text-[var(--text-primary)] rounded-xl">
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between m-6 gap-4 ">
+      <div className="dashboard h-[80vh] w-[92%] bg-background-image text-[var(--text-primary)] rounded-xl p-6">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between bg-[var(--background-color)] gap-4 p-2 text-[var(--text-color)] rounded-md">
           <h1 className="text-4xl heading-font">Weather Dashboard</h1>
           <div className="flex-1 relative">
             <form action="GET">
@@ -61,8 +81,9 @@ export default function MainPage() {
             </form>
             {searchResults.length > 1 && (
               <div className="absolute bg-[var(--text-primary)] w-full mt-2 rounded-md p-2">
-                {searchResults.map((result) => (
+                {searchResults.map((result, index) => (
                   <p
+                  onClick={() => fetchCurrentWeatherData(index)}
                     key={result.latitude + result.longitude}
                     className="text-black text-lg py-1 "
                   >
@@ -73,6 +94,9 @@ export default function MainPage() {
             )}
           </div>
         </div>
+        <main className="mt-6">
+        <TodaysWeatherCard />
+        </main>
       </div>
     </main>
   );
