6a0a632679dd08b72af93679d58f1c7c4de86c3c
Nehemiah Brown <nbrowndeveloper@gmail.com>
2026-08-06 08:49:59 -0500

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Weather Dashboard",
+        short_name: "Weather",
+        description: "Weather dashboard built with React",
+        theme_color: "#3a3d3a",
+        background_color: "#3a3d3a",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
